a7d96874005084e12c6e2be98d9b56e59138a556
mkawsar <kawsar.diu888@gmail.com>
2019-11-29 23:54:39 +0600

    Added user roles

--- user/management/commands/add_user.py
+++ user/management/commands/add_user.py
@@ -1,4 +1,5 @@
 from faker import Faker
+from user.models import Role
 from django.contrib.auth.models import User
 from django.core.management.base import BaseCommand
 
@@ -7,13 +8,17 @@ class Command(BaseCommand):
     help = 'Add new user'
 
     def handle(self, *args, **kwargs):
+        Role.objects.all().delete()
         User.objects.all().delete()
         username = 'admin'
         user = User.objects.create_user(username=username, password='123456', email='admin@admin.com',
                                         first_name='Admin', last_name='Example')
+
         user.is_superuser = False
         user.is_staff = True
         user.save()
+        role = Role(user_id=user.id, is_admin=True)
+        role.save()
         for i in range(5):
             fake = Faker()
             name = fake.first_name()
@@ -22,4 +27,6 @@ class Command(BaseCommand):
             user.is_superuser = False
             user.is_staff = True
             user.save()
+            role = Role(user_id=user.id, is_admin=False)
+            role.save()
         self.stdout.write('Added new user')
--- user/models.py
+++ user/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
 
 # Create your models here.
+class Role(models.Model):
+    user = models.OneToOneField(User, on_delete=models.DO_NOTHING)
+    is_admin = models.BooleanField(default=False)
+    createdAt = models.DateTimeField(default=timezone.now)
+    updatedAt = models.DateTimeField(default=timezone.now)
+
+    class Meta:
+        db_table = 'roles'
